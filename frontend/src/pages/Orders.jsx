2c1ae61c64bf07d572330cf63ac2938cc4394573
Alifia <l200210029@student.ums.ac.id>
2025-05-03 08:37:25 +0700

    edit orders

--- frontend/src/pages/Orders.jsx
+++ frontend/src/pages/Orders.jsx
@@ -14,7 +14,7 @@ const Orders = () => {
       if (!token) {
         return null
       }
-      const response = await axios.post(backendUrl + "/api/order/userorders", {}, { headers: { token } })
+/*       const response = await axios.post(backendUrl + "/api/order/userorders", {}, { headers: { token } })
       if (response.data.success) {
         let allOrdersItem = []
         response.data.orders.map((order)=> {
@@ -32,8 +32,55 @@ const Orders = () => {
       console.log(error)
       toast.error(error.message)
     }
-  }
-
+  } */
+  {orders.map((order) => (
+    <div key={order._id} className='grid grid-cols-1 sm:grid-cols-[0.5fr_2fr_1fr] lg:grid-cols-[0.5fr_2fr_1fr_0.5fr_1fr] gap-4 items-start p-3 text-gray-700 bg-white rounded-lg'>
+      <div className='flexCenter'>
+        <TfiPackage className='text-3xl text-secondary'/>
+      </div>
+      <div>
+        <div className='flex items-start gap-1'>
+          <div className='medium-14'>Items:</div>
+          <div className='flex flex-col relative top-0.5'>
+            {order.items.map((item, index) => {
+              if(index === order.items.length - 1) {
+                return <p key={index}>
+                  {item.name} x {item.quantity} <span>"
+                  {item.size}"</span>
+                </p>
+              } else {
+                return <p key={index}>
+                  {item.name} x {item.quantity} <span>"
+                  {item.size}"</span> ,
+                </p>
+              }
+            })}
+          </div>
+        </div>
+        <p className='medium-14'><span className='text-tertiary'>Name: </span>
+        {order.address.firstName + " " + order.address.lastName}</p>
+        <p className='medium-14'><span className='text-tertiary'>Address: </span>
+          <span>{order.address.street + ", " }</span>
+          <span>{order.address.city + ", " + order.address.state + ", " + order.address.country + ", " + order.address.zipcode}</span>
+        </p>
+        <p>{order.address.phone}</p>
+      </div>
+      <div>
+        <p className='text-sm'>Total: {order.items.length}</p>
+        <p className='mt-3'>Method: {order.paymentMethod}</p>
+        <p>Payment: {order.payment ? "Done" : "Pending"}</p>
+        <p>Date: {new Date(order.date).toLocaleDateString()}</p>
+      </div>
+      <p className='text-sm font-semibold'>Rp{order.amount}.000</p>
+      <select onChange={(e) => statusHandler(e, order._id)} value={order.status} className='text-xs font-semibold p-1 ring-1 ring-slate-900/5 rounded max-w-36 bg-primary'>
+        <option value="Pesanan Diterima">Pesanan Diterima</option>
+        <option value="Pengemasan">Pengemasan</option>
+        <option value="Dikirim">Dikirim</option>
+        <option value="Dalam Pengiriman">Dalam Pengiriman</option>
+        <option value="Terkirim">Terkirim</option>
+      </select>
+    </div>
+  ))}
   //Temporary data
   useEffect(() => {
     loadOrderData()
